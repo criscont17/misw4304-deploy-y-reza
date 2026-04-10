5a006008c1d9b662bd105419b37b417c108ce0b6
maycolan <m.avendanon@uniandes.edu.co>
2026-04-10 00:09:17 -0500

    se ajusta codigo para integrar rds, aprovisionamiento en aws beanstalk y resolucion de conflictos de librerias

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ def expired_token_callback(jwt_header, jwt_payload):
 
 
 app = create_app()
-
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
